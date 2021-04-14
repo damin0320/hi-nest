0d12db1517d2bb64412f62c79fc801a6f57e8f98
damin0320 <damin0320@gmail.com>
2021-04-14 14:18:59 +0900

    ADD : nestJS study hi-nest

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './movies/app.controller';
+
+
+@Module({
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,6 @@
+import { IsNumber, IsString } from "class-validator";
+import {PartialType} from "@nestjs/mapped-types";
+import {CreateMovieDto} from './create-movie.dto';
+
+export class UpdateMovieDto extends PartialType(CreateMovieDto){
+}
